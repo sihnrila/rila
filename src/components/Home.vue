e51a638aa0ae83aec2a510ef2eed4559439f58df
shinrila <oo8923@gmail.com>
2022-06-19 05:03:15 +0900

    new

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -290,11 +290,12 @@
                                         <img src="../assets/img/sub_4_m_1.jpg" class="d-block w-100" alt="...">
                                       </div>
                                     </div>
-                                    
+
                                     <button class="carousel-control-prev" type="button" data-bs-target="#web_silde_02" data-bs-slide="prev">
                                       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                       <span class="visually-hidden">Previous</span>
                                     </button>
+                                    
                                     <button class="carousel-control-next" type="button" data-bs-target="#web_silde_02" data-bs-slide="next">
                                       <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                       <span class="visually-hidden">Next</span>
