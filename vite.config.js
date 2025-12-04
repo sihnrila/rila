9d212f9ec9fcfde08f9a9b20629d3f133150d36c
rrsin <oo8923@boinit.com>
2025-12-04 15:09:38 +0900

    Vite 설정 업데이트: JSX 런타임 자동 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxRuntime: 'automatic'
+  })],
   base: process.env.NODE_ENV === 'production' ? '/rilaa' : '/',
   css: {
     preprocessorOptions: {
